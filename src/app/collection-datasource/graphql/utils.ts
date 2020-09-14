a021b7d11fdc5f9212e2c2cca7fcef32861c0dd7
Hieu Tran <trantrunghieu3010@gmail.com>
2020-09-15 01:22:18 +0700

    Support search non unicode

--- src/app/collection-datasource/graphql/utils.ts
+++ src/app/collection-datasource/graphql/utils.ts
@@ -20,29 +20,42 @@ export const parseQueryCriteriaToGraphQLDoorQuery = (
   queryCriteria: QueryCriteria[],
   isSearchOr?: boolean,
 ) => {
+  const getQueryString = (criteria: QueryCriteria) => {
+    const { field, operator, value } = criteria;
+    switch (operator) {
+      case GRAPHQL_QUERY_OPERATOR.CONTAINS:
+        return `${field}.ToLower().Contains(${formatCriteriaValue(criteria)})`;
+      case GRAPHQL_QUERY_OPERATOR.EQUALS:
+      case GRAPHQL_QUERY_OPERATOR.NOT_EQUAL:
+      case GRAPHQL_QUERY_OPERATOR.GT:
+      case GRAPHQL_QUERY_OPERATOR.GTE:
+      case GRAPHQL_QUERY_OPERATOR.LT:
+      case GRAPHQL_QUERY_OPERATOR.LTE:
+        return `${field} ${operator} ${formatCriteriaValue(criteria)}`;
+      case GRAPHQL_QUERY_OPERATOR.ANY_FALSE:
+        return `!${field}.Any(s => s.${value})`;
+      case GRAPHQL_QUERY_OPERATOR.ANY_TRUE:
+        return `${field}.Any(s => s.${value})`;
+      default:
+        return '';
+    }
+  };
+
   if (!isEmpty(queryCriteria)) {
     const parsedFilters = queryCriteria.map((criteria: QueryCriteria) => {
       const { field, operator, value } = criteria;
+      if (field.includes('@')) {
+        const fieldOrs = field.split('@');
+        const orQueries: string[] = [];
+        fieldOrs.forEach((orField: string) => {
+          const query = getQueryString({ field: orField, operator, value });
+          orQueries.push(query);
+        });
 
-      switch (operator) {
-        case GRAPHQL_QUERY_OPERATOR.CONTAINS:
-          return `${field}.ToLower().Contains(${formatCriteriaValue(
-            criteria,
-          )})`;
-        case GRAPHQL_QUERY_OPERATOR.EQUALS:
-        case GRAPHQL_QUERY_OPERATOR.NOT_EQUAL:
-        case GRAPHQL_QUERY_OPERATOR.GT:
-        case GRAPHQL_QUERY_OPERATOR.GTE:
-        case GRAPHQL_QUERY_OPERATOR.LT:
-        case GRAPHQL_QUERY_OPERATOR.LTE:
-          return `${field} ${operator} ${formatCriteriaValue(criteria)}`;
-        case GRAPHQL_QUERY_OPERATOR.ANY_FALSE:
-          return `!${field}.Any(s => s.${value})`;
-        case GRAPHQL_QUERY_OPERATOR.ANY_TRUE:
-          return `${field}.Any(s => s.${value})`;
-        default:
-          return '';
+        return `(${orQueries.join(' || ')})`;
       }
+
+      return getQueryString(criteria);
     });
 
     return parsedFilters.join(isSearchOr ? ' || ' : ' && ');
